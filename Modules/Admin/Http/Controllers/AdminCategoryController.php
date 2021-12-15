ce5736905b67c756dc9ba3ede126890c1cffa3d9
namtruong160699 <truongmadridista2605@gmail.com>
2021-12-15 17:00:18 +0700

    Chỉnh sửa danh mục ở admin

--- Modules/Admin/Http/Controllers/AdminCategoryController.php
+++ Modules/Admin/Http/Controllers/AdminCategoryController.php
@@ -2,7 +2,6 @@
 
 namespace Modules\Admin\Http\Controllers;
 
-use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use App\Models\Category;
@@ -10,10 +9,6 @@ use Illuminate\Support\Facades\Log;
 
 class AdminCategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * @return Renderable
-     */
     public function index()
     {
         $categories = Category::select('id','name','title_seo','active','show_homepage')->get();
@@ -23,65 +18,27 @@ class AdminCategoryController extends Controller
         return view('admin::categories.index', $viewData);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
     public function create()
     {
         return view('admin::categories.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
-     */
     public function store(Request $request)
     {
         $this->insertOrUpdate($request);
         return redirect()->route('admin.get.list.category');
     }
 
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function show($id)
-    {
-        return view('admin::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
     public function edit($id)
     {
-        return view('admin::edit');
+        $category = Category::find($id);
+        return view('admin::categories.update', compact('category'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
     public function update(Request $request, $id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
-    public function destroy($id)
-    {
-        //
+        $this->insertOrUpdate($request, $id);
+        return redirect()->route('admin.get.list.category');
     }
 
     public function insertOrUpdate($request, $id='')
@@ -98,6 +55,7 @@ class AdminCategoryController extends Controller
             $category->icon = str_slug($request->icon);
             $category->title_seo = $request->title_seo ? $request->title_seo : $request->name;
             $category->description_seo = $request->description_seo;
+            $category->color = $request->color;
             $category->save();
         } catch (\Exception $exception) {
             $code = 0;
--- Modules/Admin/Resources/views/categories/update.blade.php
+++ Modules/Admin/Resources/views/categories/update.blade.php
@@ -1,5 +1,5 @@
 @extends('admin::layouts.master',[
-    'page_title' => 'Thêm mới danh mục',
+    'page_title' => 'Cập nhật danh mục',
     'menu_open' => 'category',
     'current_menu' => 'category',
 ])
@@ -11,7 +11,7 @@
                 <li class="breadcrumb-item">
                     <a href="#">Danh mục</a>
                 </li>
-                <li class="breadcrumb-item active" aria-current="page">Thêm mới</li>
+                <li class="breadcrumb-item active" aria-current="page">Cập nhật</li>
             </ol>
         </nav>
     </div>
@@ -19,7 +19,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="grid">
-                    <p class="grid-header">Thêm mới danh mục</p>
+                    <p class="grid-header">Cập nhật danh mục</p>
                     <div class="grid-body">
                         <div class="item-wrapper">
                             <form action="" method="POST" enctype="multipart/form-data">
@@ -28,31 +28,31 @@
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Tên danh mục</label>
-                                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+                                            <input type="text" class="form-control" id="name" name="name" value="{{$category->name}}" placeholder="Enter your name">
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Icon</label>
-                                            <input type="text" class="form-control" placeholder="fa fa-home" name="icon">
+                                            <input type="text" class="form-control" placeholder="fa fa-home" value="{{$category->icon}}" name="icon">
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Mã màu</label>
-                                            <input type="color" class="form-control" name="color" value="#ed6868">
+                                            <input type="color" class="form-control" name="color" value="{{old('color',isset($category->color) ? $category->color : '#ed6868')}}">
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Meta Title</label>
-                                            <input type="text" class="form-control" placeholder="Meta Title" name="title_seo">
+                                            <input type="text" class="form-control" placeholder="Meta Title" value="{{$category->title_seo}}" name="title_seo">
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Meta Description</label>
-                                            <input type="text" class="form-control" placeholder="Meta Description" name="description_seo">
+                                            <input type="text" class="form-control" placeholder="Meta Description" value="{{$category->description_seo}}" name="description_seo">
                                         </div>
                                     </div>
                                 </div>
